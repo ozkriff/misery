1cde2b238fe4f7a2f1f551a74292553e10d79c15
ozkriff <ozkriff@gmail.com>
2013-05-23 16:31:12 +0400

    test_misc.py: Added unit tests for pretty_print

--- test_misc.py
+++ test_misc.py
@@ -31,7 +31,7 @@ class TestPrettyPrinter(unittest.TestCase):
         expected_output = '[]'
         real_output = misc.pretty_print(input_data)
         self.assertEqual(expected_output, real_output)
-        
+
     def test_empty_map(self):
         ''' Print empty map. '''
         input_data = {}
@@ -39,5 +39,73 @@ class TestPrettyPrinter(unittest.TestCase):
         real_output = misc.pretty_print(input_data)
         self.assertEqual(expected_output, real_output)
 
+    def test_map_1(self):
+        ''' Print map with one field. '''
+        input_data = { 'field':1 }
+        expected_output = (
+            '{\n'
+            '    field: 1\n'
+            '}'
+        )
+        real_output = misc.pretty_print(input_data)
+        self.assertEqual(expected_output, real_output)
+
+    def test_map_2(self):
+        ''' Print map with two fields. '''
+        input_data = { '1':1, '2':2 }
+        expected_output = (
+            '{\n'
+            '    1: 1\n'
+            '    2: 2\n'
+            '}'
+        )
+        real_output = misc.pretty_print(input_data)
+        self.assertEqual(expected_output, real_output)
+
+    def test_empty_tuple(self):
+        ''' Print empty tuple. '''
+        input_data = ()
+        expected_output = (
+            '<TUPLE>(\n'
+            ')'
+        )
+        real_output = misc.pretty_print(input_data)
+        self.assertEqual(expected_output, real_output)
+
+    def test_simple_tuple(self):
+        ''' Print simple tuple. '''
+        input_data = (1, 2, 3)
+        expected_output = (
+            '<TUPLE>(\n'
+            '    1,\n'
+            '    2,\n'
+            '    3,\n'
+            ')'
+        )
+        real_output = misc.pretty_print(input_data)
+        self.assertEqual(expected_output, real_output)
+
+    def test_string(self):
+        ''' Print string. '''
+        input_data = 'hi'
+        expected_output = '\"hi\"'
+        real_output = misc.pretty_print(input_data)
+        self.assertEqual(expected_output, real_output)
+
+    def test_float(self):
+        ''' Print floating point number. '''
+        input_data = 1.1
+        expected_output = '1.1'
+        real_output = misc.pretty_print(input_data)
+        self.assertEqual(expected_output, real_output)
+
+    def test_empty_object(self):
+        ''' Print object wothout fields. '''
+        class TestClass:
+            pass
+        input_data = TestClass()
+        expected_output = 'TestClass()'
+        real_output = misc.pretty_print(input_data)
+        self.assertEqual(expected_output, real_output)
 
 # vim: set tabstop=4 shiftwidth=4 softtabstop=4 expandtab:
